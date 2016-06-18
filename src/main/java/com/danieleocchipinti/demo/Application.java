f6f3453bb14a3385ef3e3cad90edb6a218704044
Plancake <services@plancake.com>
2016-06-18 22:30:20 +0100

    added some specific Spring Boot annotations for the Application class

--- src/main/java/com/danieleocchipinti/demo/Application.java
+++ src/main/java/com/danieleocchipinti/demo/Application.java
@@ -5,13 +5,16 @@ import org.springframework.boot.autoconfigure.*;
 import org.springframework.stereotype.*;
 import org.springframework.web.bind.annotation.*;
 
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
 @RestController
-@EnableAutoConfiguration
+@SpringBootApplication // same as @Configuration @EnableAutoConfiguration @ComponentScan
 public class Application {
 
     @RequestMapping("/")
     String home() {
-        return "Hello World 5!";
+        return "Hello World 6!";
     }
 
     public static void main(String[] args) throws Exception {
