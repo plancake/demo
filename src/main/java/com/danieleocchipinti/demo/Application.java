8960fe25d06d0f9ee1d4a610e2d6fd445b77b6b7
Plancake <services@plancake.com>
2016-06-19 17:34:42 +0100

    added handling of different profiles: dev and prod

--- src/main/java/com/danieleocchipinti/demo/Application.java
+++ src/main/java/com/danieleocchipinti/demo/Application.java
@@ -1,5 +1,8 @@
 package com.danieleocchipinti.demo;
 
+import javax.annotation.PostConstruct;
+
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.*;
 import org.springframework.boot.autoconfigure.*;
 import org.springframework.stereotype.*;
@@ -11,18 +14,30 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @RestController
 @SpringBootApplication // same as @Configuration @EnableAutoConfiguration @ComponentScan
 public class Application {
+	
+	@Value("${spring.profiles.active}")
+	private String activeProfile;
 
     @RequestMapping("/")
     String home() {
-        return "Hello World 6!";
+        return String.format("Hello World from profile %s!", this.activeProfile);
     }
 
     public static void main(String[] args) throws Exception {
+    	
     	SpringApplication app = new SpringApplication(Application.class);
     	
     	app.setBannerMode(Banner.Mode.OFF);
     	
         app.run(args);
+    }
+    
+    @PostConstruct
+    public void init()
+    {
+    	if (this.activeProfile.equals("default")) {
+    		throw new RuntimeException("A profile needs to be passed on the command line on start - e.g. -Dspring.profiles.active=dev");    		
+    	}
     }
 
 }
