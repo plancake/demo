04391545b9eb3c40720c310b9261416328bdfdfa
Plancake <services@plancake.com>
2016-06-27 10:36:21 +0100

    improved the html markup

--- public/js/account.js
+++ public/js/account.js
@@ -1,3 +1,17 @@
 $( document ).ready(function() {
-    
+	
+	if ($('#buyerDealsList').length) {
+		$('#buyerDealsList a').click(function (e) {
+			
+			// $("#dialog iframe").attr('src', $(this).attr('hred')).dialog();
+			
+			
+			
+			$("#dialog").show();
+			
+			e.preventDefault();
+			return false;
+		});
+	}
+	
 });
